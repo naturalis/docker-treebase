4d8b63bf0e17680808f35a86f7243c104aecbdfd
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2018-03-09 11:19:19 +0100

    updated docker files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # edit config
-/bin/sed -i -E "s/url=\"jdbc:postgresql:.*/url=\"jdbc:postgresql:$POSTGRES_HOST\/$POSTGRES_DB\"/" /usr/local/tomcat/webapps/treebase-web/META-INF/context.xml
+/bin/sed -i -E "s/url=\"jdbc:postgresql:.*/url=\"jdbc:postgresql:\/\/$POSTGRES_HOST\/$POSTGRES_DB\"/" /usr/local/tomcat/webapps/treebase-web/META-INF/context.xml
 /bin/sed -i -E "s/username=\".*/username=\"$POSTGRES_USER\" password=\"$POSTGRES_PASSWORD\"/" /usr/local/tomcat/webapps/treebase-web/META-INF/context.xml
+/bin/sed -i -E "s/Environment name=\"tb2\/SiteUrl\".*/Environment name=\"tb2\/SiteUrl\" value=\"http:\/\/$siteUrl\/treebase-web\/\"/" /usr/local/tomcat/webapps/treebase-web/META-INF/context.xml
 
 # run server
 $CATALINA_HOME/bin/catalina.sh run
